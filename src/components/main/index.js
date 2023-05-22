1c940b9c97da0f4df96cc2f982924fb2e23b5abd
Luis Chrestia <chrestia.luis@gmail.com>
2023-05-22 20:00:02 -0300

    se comenta linea 5 index.js del main

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import './main.css'
 import { getPokemons, searchPokemon } from "../../redux/actions";
-import { GET_POKEMONS } from "../../redux/types";
+//import { GET_POKEMONS } from "../../redux/types";
 
 
 export const Main = () => {
